df75a0a3403812cea68faaceee04e2b702159718
Atharva Athanikar <120152105+AtharvaAthanikar2003@users.noreply.github.com>
2025-01-07 14:12:33 +0530

    Delete EV_Web_Dev_Projects directory

--- EV_Web_Dev_Projects/JS/Atharva Athanikar 72287317E JS-1/Storage API.js
+++
@@ -1,7 +0,0 @@
-// Save patient data to localStorage
-function savePatientData(newPatient) {
-    const patients = localStorage.getItem("patients") ? JSON.parse(localStorage.getItem("patients")) : [];
-    patients.push(newPatient);
-    localStorage.setItem("patients", JSON.stringify(patients));
-}
-displayPatients();
